2748bb53707eb4e313e50836fdf4e9c810a09d90
Gokul_S <gokulsarav2005@gmail.com>
2025-07-27 20:24:37 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/portfolio">
           <Layout>
             <Portfolio />
           </Layout>
